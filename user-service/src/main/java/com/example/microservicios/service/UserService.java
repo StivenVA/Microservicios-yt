9410ac54c6cfc21d6bc7fe5ca9548c6caf6fc575
stivenVA <stivenv.ariza@gmail.com>
2023-07-30 11:52:11 -0500

    correccion

--- user-service/src/main/java/com/example/microservicios/service/UserService.java
+++ user-service/src/main/java/com/example/microservicios/service/UserService.java
@@ -1,14 +1,10 @@
 package com.example.microservicios.service;
 
 import com.example.microservicios.entity.User;
-import com.example.microservicios.feignclients.BikeFeignClient;
-import com.example.microservicios.feignclients.CarFeignClient;
-import com.example.microservicios.model.Bike;
-import com.example.microservicios.model.Car;
 import com.example.microservicios.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
 
 import java.util.List;
 
@@ -17,9 +13,6 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final RestTemplate restTemplate;
-    private final CarFeignClient carClient;
-    private final BikeFeignClient bikeFeignClient;
 
     public List<User> getAll(){
         return userRepository.findAll();
@@ -31,33 +24,6 @@ public class UserService {
 
     public User save(User user){
         return userRepository.save(user);
-    }
-
-    public Car saveCar(int userId,Car car){
-
-        car.setUserId(userId);
-        return carClient.save(car);
-    }
-
-    public Bike saveBike(int userId,Bike bike){
-        bike.setUserId(userId);
-        return bikeFeignClient.save(bike);
-    }
-
-    public List<Car> getAllCars(){
-        return carClient.getAll();
-    }
-
-    public List<Car> getCarsById(int userId){
-        return carClient.getById(userId);
-    }
-
-    public List<Bike> getAllBikes(){
-        return bikeFeignClient.getAll();
-    }
-
-    public List<Bike> getBikesById(int userId){
-        return bikeFeignClient.getById(userId);
     }
 
 }

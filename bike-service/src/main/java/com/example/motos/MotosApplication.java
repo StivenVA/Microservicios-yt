f15785134fc5dff8a777fe7042844502cb9ff587
stivenVA <stivenv.ariza@gmail.com>
2023-07-30 14:26:44 -0500

    Eureka

--- bike-service/src/main/java/com/example/motos/MotosApplication.java
+++ bike-service/src/main/java/com/example/motos/MotosApplication.java
@@ -2,8 +2,10 @@ package com.example.motos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MotosApplication {
 
     public static void main(String[] args) {
--- car-service/src/main/java/com/example/carros/CarrosApplication.java
+++ car-service/src/main/java/com/example/carros/CarrosApplication.java
@@ -2,8 +2,10 @@ package com.example.carros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CarrosApplication {
 
     public static void main(String[] args) {
